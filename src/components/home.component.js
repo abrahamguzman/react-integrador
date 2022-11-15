95157b9e6d583f227af9acfb21c25a30f935d39c
Abraham Guzman <85902876+abrahamguzman@users.noreply.github.com>
2022-11-15 00:55:55 -0500

    8vo commit -> mguzman

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Navigate } from "react-router-dom";
+import AuthService from "../services/auth.service";
 
 import UserService from "../services/user.service";
 
@@ -7,11 +9,16 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      content: ""
+      redirect: null,
+      userReady: false,
+      currentUser: { username: "" }
     };
   }
 
   componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+    if (!currentUser) this.setState({ redirect: "/home" });
+    this.setState({ currentUser: currentUser, userReady: true })
     UserService.getPublicContent().then(
       response => {
         this.setState({
@@ -30,9 +37,21 @@ export default class Home extends Component {
   }
 
   render() {
+    if (this.state.redirect) {
+      return <Navigate to={this.state.redirect} />
+    }
+
+    const { currentUser } = this.state;
+
     return (
       <div className="container">
- 
+        <header className="jumbotron">
+          {(this.state.userReady)}
+          <h3>
+            Bienvenido <br /> 
+            <strong>{currentUser.username}</strong> !!! 
+          </h3>
+        </header>
       </div>
     );
   }
